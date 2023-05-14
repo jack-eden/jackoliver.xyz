cfd5c7192fcabb4ba5dacc5c45a0e5307bbf75c0
Jack Eden <jack@liesandlibel.uk>
2023-05-15 00:05:00 +0100

    changed time to rgb function to remove hard transition between colours between 59 and 0 seconds

--- assets/js/xyz.js
+++ assets/js/xyz.js
@@ -1,16 +1,16 @@
 function setDate() {
     const now = new Date();
-  
-    const seconds = now.getSeconds(); // returns an integer, between 0 and 59
-    const blueSeconds = (seconds + 1) * 4; // change range to 1 - 60, then change to 4 - 240
 
-    const mins = now.getMinutes(); // returns an integer, between 0 and 59
-    const greenMinutes = (mins + 1) * 4;
-  
     const hours = now.getHours(); // returns an integer, between 0 and 23
     const redHours = (hours + 1) * 10 // change range to 1 - 24, then change to 10 - 240
 
-    document.body.style.backgroundColor = "rgb("+redHours+", "+greenMinutes+", "+blueSeconds+")"
+    const minutes = now.getMinutes(); // returns an integer, between 0 and 59
+    const greenMinutes = (minutes + 1) * 4;
+
+    const seconds = now.getSeconds(); // returns an integer, between 0 and 59
+    const blueSeconds = (Math.abs(seconds - 30) +1) * 8; // Turns second into an ossilating number between 1 and 31, multiplied by 8
+
+    document.body.style.backgroundColor = "rgb("+redHours+", "+greenMinutes+", "+blueSeconds+")";
 }
 
 setInterval(setDate, 1000);
